532e703a087ca6ece1881e1199817ada60d6279d
Wilmer Jaramillo M <wilmer5@gmail.com>
2012-05-02 09:10:26 -0430

    minor fix

--- db-pgstat.pl
+++ db-pgstat.pl
@@ -1,7 +1,19 @@
 #!/bin/env perl
-#
-# Wilmer Jaramillo M. <wilmer@fedoraproject.org>
-#
+# This program was designed to work with Zabbix to keep monitored PostgreSQL databases.
+# Copyright (C) 2012 Wilmer Jaramillo M. <wilmer@fedoraproject.org>
+
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>
 
 use strict;
 use DBI;
@@ -89,7 +101,6 @@ some features monitored are: connection pool, buffers, locks, checkpoints.
 Usage: $0 [--OPTION]
 Mandatory arguments:
 __EOF__
-
 
     while ( my($key, undef) = each %querys ) {
         print "\t--".$key."\n";
